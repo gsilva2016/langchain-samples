67b669b1a2085ef4b3d56c5f48cd418c24e9e1a4
gabriel-intel <g2020sudo@none.com>
2025-08-27 09:23:29 -0700

    sync repos

--- ui-demos/medical-transcribe/create-conda-env.sh
+++ ui-demos/medical-transcribe/create-conda-env.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 source .env
-
+source activate-conda.sh
+activate_conda
 echo "Creating conda environment $CONDA_ENV_NAME."
 conda create -n $CONDA_ENV_NAME python=3.11 -y
 echo 'y' | conda install pip
